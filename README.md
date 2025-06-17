a0acb07a638e81f78a8bc4ffc5d90c852fe4658e
Tom <tomgriffiths2007@gmail.com>
2025-06-17 19:50:07 +0100

    v5 update

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # apache
-This is a package for PHP-CLI that is able to manage local apache servers.
+apache is a PHP-CLI package that manages apache servers, this is not apache, just a management tool for apache servers.
 
-# Use
-To use this program in your own project:
-* Link back to original source (here or www.tomgriffiths.net).
-* Do not claim this code as your own.
+# Commands
+- **server create [httpdPath] [documentDirectory] [name]**: Creates a server with the specified settings.
+
+# Functions
+- **init**: Starts any servers with the autostart setting enabled.
+- **set_document_directory(string $httpd_conf, string $directory):bool**: Sets the document directory for a given https.conf file. Returns true on success or false on failure.
+- **start(int $serverNumber=1):bool**: Starts a specified server. Returns true on success or false on failure.
+- **stop(int $serverNumber=1):bool**: Stops a specified server. Returns true on success or false on failure.
+- **new_server(string $httpdPath, string $documentDirectory, string $name):int|bool**: Configures and installs a new apache server as a windows service. Returns the server number on success or false on failure.
+- **delete_server(int $serverNumber):bool**: Deletes a server from the internal list and uninstalls the windows service. Returns true on success or false on failure.
+- **set_autostart(int $serverNumber, bool $autostart):bool**: Sets the autostart setting for a given server. Returns true on success or false on failure.
--- main.php
+++ main.php
@@ -7,7 +7,7 @@ class apache{
         }
         foreach($servers as $serverNumber => $serverData){
             if(isset($serverData['autostart'])){
-                if($serverData['autostart'] === true){
+                if($serverData['autostart']){
                     self::start($serverNumber);
                 }
             }
@@ -23,11 +23,26 @@ class apache{
                 self::new_server($lines[3],$lines[4],$lines[2]);
             }
         }
+        else{
+            echo "Unknown command\nUsage: server create [httpdPath] [documentDirectory] [name]";
+        }
     }
-    public static function set_document_directory($httpd_conf,$directory){
+    public static function set_document_directory(string $httpd_conf, string $directory):bool{
         files::ensureFolder($directory);
-        files::copyFile($httpd_conf,$httpd_conf . "_backup");
+        if(!is_file($httpd_conf)){
+            mklog(2,'Config file not found: ' . $httpd_conf);
+            return false;
+        }
+        if(!files::copyFile($httpd_conf,$httpd_conf . "_backup")){
+            mklog(2,'Failed to create backup of config file ' . $httpd_conf);
+            return false;
+        }
         $lines = file($httpd_conf);
+        if(!is_array($lines) || empty($lines)){
+            mklog(2,'Failed to read config file ' . $httpd_conf);
+            return false;
+        }
+
         $oldDocumentRoot = "";
         $documentRootLine = false;
         $directorySettingsLine = false;
@@ -41,110 +56,182 @@ class apache{
                 $directorySettingsLine = $lineNumber;
             }
         }
+
         if($documentRootLine !== false && $directorySettingsLine !== false){
             $lines[$documentRootLine] = "DocumentRoot \"" . $directory . "\"\n";
             $lines[$directorySettingsLine] = "<Directory \"" . $directory . "\">\n";
-            file_put_contents($httpd_conf,$lines);
-        }
-    }
-    public static function start(int $serverNumber = 1){
-        if(is_admin::check()){
-            $serverInfo = settings::read("servers/" . $serverNumber);
-            return service_manager::start_service($serverInfo['serviceName']);
-        }
-        return false;
-    }
-    public static function stop(int $serverNumber = 1){
-        if(is_admin::check()){
-            $serverInfo = settings::read("servers/" . $serverNumber);
-            return service_manager::stop_service($serverInfo['serviceName']);
-        }
-        return false;
-    }
-    public static function new_server($httpdPath,$documentDirectory,$name):int|bool{
-        $httpdPath = str_replace("/","\\",$httpdPath);
-        $documentDirectory = str_replace("/","\\",$documentDirectory);
-        if(is_file($httpdPath) && is_admin::check()){
-            $serverNumber = 1;
-            while(settings::isset("servers/" . $serverNumber)){
-                $serverNumber ++;
-            }
-            $documentDirectory = str_replace("<serverNumber>",$serverNumber,$documentDirectory);
-            $serverInfo = array(
-                "serviceName"                => "php_cli_apache_server_" . $serverNumber,
-                "name"                       => $name,
-                "specifiedDocumentDirectory" => $documentDirectory,
-                "executable"                 => $httpdPath
-            );
-            settings::set("servers/" . $serverNumber,$serverInfo);
-            $httpd_conf = files::getFileDir($httpdPath) . "/../conf/httpd.conf";
-            if(is_file($httpd_conf)){
-                self::set_document_directory($httpd_conf,$documentDirectory);
+            if(file_put_contents($httpd_conf,$lines)){
+                return true;
             }
             else{
-                files::mkFile($httpd_conf,"DocumentRoot \"" . str_replace("\\","\\\\",$documentDirectory) . "\"");
+                mklog(2,'Failed to save config file ' . $httpd_conf);
+                return false;
             }
-            exec($httpdPath . ' -k install -n "' . $serverInfo['serviceName'] . '" >nul 2>&1');
+        }
+        else{
+            mklog(2,'Failed to locate DocumentRoot setting in file ' . $httpd_conf);
+            return false;
+        }
+    }
+    public static function start(int $serverNumber = 1):bool{
+        $serverInfo = settings::read("servers/" . $serverNumber);
+        if(is_array($serverInfo) && isset($serverInfo['serviceName'])){
+            return service_manager::start_service($serverInfo['serviceName']);
+        }
+        else{
+            mklog(2,'Failed to read settings for server ' . $serverNumber);
+        }
+
+        return false;
+    }
+    public static function stop(int $serverNumber = 1):bool{
+        $serverInfo = settings::read("servers/" . $serverNumber);
+        if(is_array($serverInfo) && isset($serverInfo['serviceName'])){
+            return service_manager::stop_service($serverInfo['serviceName']);
+        }
+        else{
+            mklog(2,'Failed to read settings for server ' . $serverNumber);
+        }
+
+        return false;
+    }
+    public static function new_server(string $httpdPath, string $documentDirectory, string $name):int|bool{
+        $httpdPath = str_replace("/","\\",$httpdPath);
+        $documentDirectory = str_replace("/","\\",$documentDirectory);
+
+        if(!is_admin::check()){
+            mklog(2,'Admin permission not present');
+            return false;
+        }
+        if(!is_file($httpdPath)){
+            mklog(2,'httpd.exe not found');
+            return false;
+        }
+
+        $serverNumber = 1;
+        while(settings::isset("servers/" . $serverNumber)){
+            $serverNumber ++;
+        }
+
+        $documentDirectory = str_replace("<serverNumber>",$serverNumber,$documentDirectory);
+        $serverInfo = array(
+            "serviceName"                => "php_cli_apache_server_" . $serverNumber,
+            "name"                       => $name,
+            "specifiedDocumentDirectory" => $documentDirectory,
+            "executable"                 => $httpdPath
+        );
+        if(!settings::set("servers/" . $serverNumber,$serverInfo)){
+            mklog(2,'Failed to save server information');
+            return false;
+        }
+
+        $httpd_conf = files::getFileDir(files::getFileDir($httpdPath)) . "/conf/httpd.conf";
+        if(!is_file($httpd_conf)){
+            mklog(2,'Failed to locate httpd.conf');
+            return false;
+        }
+        if(!self::set_document_directory($httpd_conf,$documentDirectory)){
+            mklog(2,'Failed to set document directory');
+            return false;
+        }
+
+        exec('"' . $httpdPath . '" -k install -n "' . $serverInfo['serviceName'] . '" >nul 2>&1', $output, $exitCode);
+
+        if($exitCode === 0){
+            mklog(1,'Created server ' . $serverNumber);
             return $serverNumber;
         }
         else{
-            mklog('warning','httpd.exe not found or not admin',false);
+            mklog(2,'Failed to install apache service');
+            return false;
         }
-        return false;
     }
     public static function delete_server(int $serverNumber):bool{
-        if(is_admin::check()){
-            $serverInfo = settings::read("servers/" . $serverNumber);
-            service_manager::stop_service($serverInfo['serviceName']);
-            settings::unset("servers/" . $serverNumber);
-            return cmd::run("sc delete " . $serverInfo['serviceName'],true,false);
+        $serverInfo = settings::read("servers/" . $serverNumber);
+        if(!is_array($serverInfo) || !isset($serverInfo['serviceName'])){
+            mklog(2,'Failed to read settings for server ' . $serverNumber);
+            return false;
         }
-        else{
-            mklog('warning','Unable to delete apache server ' . $serverNumber . ', administrator permissions required',false);
+
+        if(!service_manager::stop_service($serverInfo['serviceName'])){
+            mklog(2,'Failed to stop service');
+            return false;
         }
-        return false;
+
+        if(!settings::unset("servers/" . $serverNumber)){
+            mklog(2,'Failed to delete settings');
+            return false;
+        }
+
+        if(!service_manager::delete_service($serverInfo['serviceName'])){
+            mklog(2,'Failed to delete service');
+            return false;
+        }
+        return true;
     }
-    public static function set_autostart(int $serverNumber, bool $autostart){
-        if(settings::isset("servers/" . $serverNumber)){
-            settings::set("servers/" . $serverNumber . "/autostart",$autostart);
+    public static function set_autostart(int $serverNumber, bool $autostart):bool{
+        if(!settings::isset("servers/" . $serverNumber)){
+            mklog(2,'Failed to read server information');
+            return false;
         }
+
+        if(!settings::set("servers/" . $serverNumber . "/autostart", $autostart)){
+            mklog(2,'Failed to save server information');
+            return false;
+        }
+
+        return true;
     }
     public static function ensurePhpExtension(int $serverNumber, array $extensions):bool{
         $info = settings::read('servers/' . $serverNumber);
-        $enabledExtensions = array();
-        if(is_array($info)){
-            $phpIni = files::getFileDir(files::getFileDir(files::getFileDir($info['executable']))) . "\\php\\php.ini";
-            if(is_file($phpIni)){
-                $file = file($phpIni);
-                if(is_array($file)){
-                    foreach($file as $index => $line){
-                        $line1 = substr(trim($line),0,strpos($line . " "," "));
-                        $substr = substr($line1,0,10);
-                        if($substr === ";extension" || $substr === "extension="){
-                            foreach($extensions as $extension){
-                                $line2 = "extension=" . $extension;
-                                if($line1 === $line2){
-                                    $enabledExtensions[] = $extension;
-                                }
-                                elseif($line1 === ";" . $line2){
-                                    $file[$index] = str_replace(";","",$line);
-                                    $enabledExtensions[] = $extension;
-                                }
-                            }
-                        }
+
+        if(!is_array($info)){
+            mklog(2,'Failed to read server information');
+            return false;
+        }
+
+        $phpIni = files::getFileDir(files::getFileDir(files::getFileDir($info['executable']))) . "\\php\\php.ini";
+        if(!is_file($phpIni)){
+            mklog(2,'Failed to find php.ini');
+            return false;
+        }
+
+        $file = file($phpIni);
+        if(!is_array($file)){
+            mklog(2,'Failed to read php.ini');
+            return false;
+        }
+
+        $enabledExtensions = [];
+
+        foreach($file as $index => $line){
+            $line1 = substr(trim($line),0,strpos($line . " "," "));
+            $substr = substr($line1,0,10);
+            if($substr === ";extension" || $substr === "extension="){
+                foreach($extensions as $extension){
+                    $line2 = "extension=" . $extension;
+                    if($line1 === $line2){
+                        $enabledExtensions[] = $extension;
                     }
-                    foreach($extensions as $extension){
-                        if(!in_array($extension,$enabledExtensions)){
-                            mklog('warning',"Unable to find extension entry '" . $extension . "' in " . $phpIni);
-                            return false;
-                        }
-                    }
-                    if(file_put_contents($phpIni,$file) !== false){
-                        return true;
+                    elseif($line1 === ";" . $line2){
+                        $file[$index] = str_replace(";","",$line);
+                        $enabledExtensions[] = $extension;
                     }
                 }
             }
         }
-        return false;
+        foreach($extensions as $extension){
+            if(!in_array($extension,$enabledExtensions)){
+                mklog(2,"Unable to find extension entry '" . $extension . "' in " . $phpIni);
+                return false;
+            }
+        }
+
+        if(!file_put_contents($phpIni,$file)){
+            mklog(2,'Failed to save php.ini');
+            return false;
+        }
+        
+        return true;
     }
 }
